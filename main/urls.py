20c60f17e731882b880d66c415d937bcb255e4f8
CHESTERFILED <Michenko.nik@gmail.com>
2019-09-02 09:54:32 +0300

    Some changes in files

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,5 @@ from .views import *
 
 urlpatterns = [
     path('', get_home_page, name='home_page_url'),
+    path('cinemas/', get_cinemas_page, name='cinemas_page_url'),
 ]
--- main/views.py
+++ main/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def get_home_page(request):
     return render(request, 'main/home_page.html')
+
+
+def get_cinemas_page(request):
+    return render(request, 'main/cinemas_page.html')
