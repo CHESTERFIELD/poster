bcfac15ad911e7f284d8465b9d8cab897c35726e
CHESTERFILED <Michenko.nik@gmail.com>
2019-09-03 17:29:09 +0300

    Some changes in project

--- main/urls.py
+++ main/urls.py
@@ -5,4 +5,7 @@ from .views import *
 urlpatterns = [
     path('', get_home_page, name='home_page_url'),
     path('cinemas/', get_cinemas_page, name='cinemas_page_url'),
+    path('cinemas/cinema_city/', get_cinema_city_page, name='cinema_city_url'),
+    # path('cinemas/planeta_kino/', get_planeta_kino_page, name='planeta_kino_url'),
+    # path('cinemas/cinema_city/', get_multiplex_page, name='multiplex_url'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from urllib.request import *
+from bs4 import BeautifulSoup
+
+import http.client
+http.client._MAXHEADERS = 1000
 
 
 def get_home_page(request):
@@ -7,3 +12,40 @@ def get_home_page(request):
 
 def get_cinemas_page(request):
     return render(request, 'main/cinemas_page.html')
+
+
+def parse_genre_and_time_duration(children):
+    """parsing the genre and time of the movie to the list"""
+    set_p = children.find_all('p')
+    if set_p[0].string is None:
+        set_p[0].string = 'Неизвестно'
+    return set_p
+
+
+def get_cinema_city_page(request):
+    host = "https://cinemaciti.ua/"
+    url = "https://cinemaciti.ua/fontan-sky-center/rozklad"
+    html = urlopen(url)
+    bsObj = BeautifulSoup(html.read(), 'html.parser')
+    full_info = dict()
+    count = 0
+    for item in bsObj.findAll("div", class_="session clear"):
+        result = dict()
+        count = count + 1
+        # genre & time_duration
+        genre_time = parse_genre_and_time_duration(item)
+        children = item.findChildren(recursive=True)
+        for child in children:
+            # name
+            if child.name == "a" and 'class' in child.attrs and child.attrs['class'][0] == 'session__movie-name':
+                name = child.contents[0]
+            # link
+            if child.name == "a" and 'class' in child.attrs and child.attrs['class'][0] == 'session__movie-name':
+                link = host + child.attrs['href']
+
+        full_info[count] = result
+        result['genre_time'] = genre_time
+        result['name'] = name
+        result['link'] = link
+
+    return render(request, 'main/cinema_city.html', context={'full_info': full_info})
