9b7261609e72f32c770a7a871a869feb54b8de34
CHESTERFILED <Michenko.nik@gmail.com>
2019-10-16 21:18:22 +0300

    fix

--- telegram_bot/views.py
+++ telegram_bot/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
 import telebot
 
-bot = telebot.TeleBot("886807439:AAFXCpSd3YkKdeaJ1a1tgXRCYLrfz0oph4U")
 
-
-@bot.message_handler(commands=['start'])
-def start_message(message):
-    bot.send_message(message.chat.id, 'Hello my friend, you write me "/start"')
-
-
-bot.polling()
+#
+# @bot.message_handler(commands=['start'])
+# def start_message(message):
+#     bot.send_message(message.chat.id, 'Hello my friend, you write me "/start"')
+#
+#
+# bot.polling()
